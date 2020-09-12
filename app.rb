fc52c2b054800b12eb1728ef73bac7eb8440216f
zachary mines <zmines@wesleyan.edu>
2020-09-12 19:38:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,18 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	get '/hello' do 
+	  erb :hello 
+	end 
+	
+	get '/goodbye' do 
+	  erb :goodbye 
+	end 
+	
+	get '/date' do 
+	  erb :date
+	end 
 
 
 end
